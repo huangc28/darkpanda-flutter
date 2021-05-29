59fda0d69458c6dff18a3aa8ad05b6bf411cbd6b
ykssheng <ykssheng@gmail.com>
2021-05-29 17:17:24 +0800

    male - search inquiry, inquiry form, waiting inquiry

--- lib/screens/service_chatroom/components/service_chatroom_grid.dart
+++ lib/screens/service_chatroom/components/service_chatroom_grid.dart
@@ -1,4 +1,5 @@
 import 'package:darkpanda_flutter/screens/service_chatroom/models/incoming_service.dart';
+import 'package:darkpanda_flutter/util/time_remaining_since_date.dart';
 import 'package:flutter/material.dart';
 
 import 'package:darkpanda_flutter/components/user_avatar.dart';
@@ -19,9 +20,10 @@ class ServiceChatroomGrid extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 10),
       decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
         color: Color.fromRGBO(31, 30, 56, 1),
         border: Border.all(
-          width: 1,
+          width: 0.5,
           color: Color.fromRGBO(106, 109, 137, 1),
         ),
       ),
@@ -48,29 +50,77 @@ class ServiceChatroomGrid extends StatelessWidget {
   }
 
   Widget _buildChatInfoBar() {
+    DateTime today = DateTime.now();
+    today = today.add(Duration(hours: 10));
     return Row(
       children: [
-        Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                chatroom.username,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 5.0,
+              vertical: 10.0,
+            ),
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    chatroom.username,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    lastMessage,
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Color.fromRGBO(106, 109, 137, 1),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 6),
-              Text(
-                lastMessage,
-                style: TextStyle(
-                  fontSize: 13,
-                  color: Color.fromRGBO(106, 109, 137, 1),
-                ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
+            ),
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    DateTimeUtil.timeAgoSinceDate(today),
+                    style: TextStyle(
+                      color: Color.fromRGBO(106, 109, 137, 1),
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(4.5, 1, 4.5, 1),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Color.fromRGBO(236, 97, 88, 1),
+                    ),
+                    child: Text(
+                      '1',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ],
