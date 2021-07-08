1c51a4c5a447421704d87233613d2e4021008104
Chi-Han Huang <huangchihan@gmail.com>
2021-07-08 23:27:21 +0800

    Append environment variable when building android APK.

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -22,7 +22,15 @@ echo "Installed flutter to `pwd`/flutter"
 # if you get "Execution failed for task ':app:lintVitalRelease'." error, uncomment next two lines
 # flutter build apk --debug
 # flutter build apk --profile
-flutter build apk --release
+flutter build \
+    --dart-define=SERVER_HOST=$SERVER_HOST \
+    --dart-define=PUBNUB_PUBLISH_KEY=$PUBNUB_PUBLISH_KEY \
+    --dart-define=PUBNUB_SUBSCRIBE_KEY=$PUBNUB_SUBSCRIBE_KEY \
+    --dart-define=PUBNUB_SECRET_KEY=$PUBNUB_SECRET_KEY \
+    --dart-define=GEOCODING_APIS=$GEOCODING_APIS \
+    --dart-define=APPCENTER_ANDROID_APP_SECRET=$APPCENTER_ANDROID_APP_SECRET \
+    --dart-define=ENV=$ENV \
+    apk --release
 
 # if you need build bundle (AAB) in addition to your APK, uncomment line below and last line of this script.
 #flutter build appbundle --release --build-number $APPCENTER_BUILD_ID
