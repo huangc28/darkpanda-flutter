9bf2cd09f77d1563792f8b4ebb2482d19c398db5
ykssheng <ykssheng@gmail.com>
2021-08-27 09:34:47 +0800

    Fix bug

--- lib/screens/chatroom/screens/service/screens/components/cancel_service_confirmation_dialog.dart
+++ lib/screens/chatroom/screens/service/screens/components/cancel_service_confirmation_dialog.dart
@@ -39,25 +39,31 @@ class CancelServiceConfirmationDialog extends StatelessWidget {
           ),
         ),
         actions: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width * 0.3,
-            child: DPTextButton(
-              theme: DPTextButtonThemes.grey,
-              onPressed: () async {
-                Navigator.pop(context, false);
-              },
-              text: '不取消',
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.3,
-            child: DPTextButton(
-              theme: DPTextButtonThemes.purple,
-              onPressed: () async {
-                Navigator.pop(context, true);
-              },
-              text: '確定取消',
-            ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 0.3,
+                child: DPTextButton(
+                  theme: DPTextButtonThemes.grey,
+                  onPressed: () async {
+                    Navigator.pop(context, false);
+                  },
+                  text: '不取消',
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.3,
+                child: DPTextButton(
+                  theme: DPTextButtonThemes.purple,
+                  onPressed: () async {
+                    Navigator.pop(context, true);
+                  },
+                  text: '確定取消',
+                ),
+              ),
+            ],
           ),
         ],
       ),
