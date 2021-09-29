aa38261484da0ea296b2abf08900ef57334e76c7
Chi-Han Huang <huangchihan@gmail.com>
2021-09-29 12:04:19 +0800

    Try to fix cicd 4.Try to fix cicd 4.

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -14,8 +14,8 @@ git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 
 # switch flutter channel to 'stable' and upgrade to latest build
-# flutter channel stable
-flutter channel dev
+flutter channel stable
+flutter upgrade
 
 # accepting all licenses
 yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
