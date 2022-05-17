018ba9c6ed8e1d1556a07dbbade2ddc86bb98b6e
ykssheng <ykssheng@gmail.com>
2022-05-17 11:29:08 +0800

    Update appcenter-post-clone.sh

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -15,7 +15,7 @@ export PATH=`pwd`/flutter/bin:$PATH
 
 # switch flutter channel to 'stable' and upgrade to latest build
 flutter channel stable
-flutter downgrade 2.10.3
+flutter downgrade v2.10.3
 
 # accepting all licenses
 yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
